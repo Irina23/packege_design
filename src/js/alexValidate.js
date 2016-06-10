e3bf73ce5d70c50ad8d97a1b47b09590cb087ab1
ira <bakum21@ukr.net>
2016-06-10 15:54:06 +0300

    init

---
+++ src/js/alexValidate.js
@@ -0,0 +1,85 @@
+(function ($) {
+
+	$.fn.validate = function (opt) {
+
+		this.each(function (i) {
+
+			var DOM = {},
+				state = {},
+				$self = $(this);
+
+			// options
+			if (!opt) {
+				opt = {};
+			}
+			opt = $.extend({
+			}, opt);
+
+			// methods
+			var plg = {
+				init: function () {
+					DOM.$fields = $self.find('[data-validate]');
+					$self.find('.btn.submit').on('click', plg.submit);
+					DOM.$fields.on('focus', function () {
+						plg.removeLabel( $(this) );
+					})
+				},
+
+				test: function (data, type) {
+
+					switch (type) {
+						case 'name':
+							return /^[а-яіїєґёА-ЯІЇЄҐЁa-zA-Z\-]+\s{0,1}[а-яіїєґёА-ЯІЇЄҐЁa-zA-Z\-]{0,}$/.test(data);
+						case 'phone':
+							return /^[\(\)0-9\-\s\+]{6,}/.test(data);
+						case 'email':
+							return /^[0-9a-zA-Z._-]+@[0-9a-zA-Z_-]+\.[a-zA-Z._-]+/.test(data);
+						case 'number':
+							return /^[0-9]/.test(data);
+						case 'empty':
+							return /^[а-яіїєґёА-ЯІЇЄҐЁa-zA-Z0-9]+/.test(data);
+						case 'select':
+							return !(data === null || data === "null" || data === "false" || data === "0" || data === "undefined");
+						//case 'radio':
+							//return $('input[type="radio"]:checked').length==0;
+							//return $('input[type="radio"]:checked').val();
+						case 'quantity':
+							return /^[1-9]/.test(data);
+						default:
+							return true;
+					}
+				},
+				addLabel: function ($el) {
+					$el.parent().addClass('error');
+				},
+				removeLabel: function ($el) {
+					$el.parent().removeClass('error');
+				},
+				validate: function ($el) {
+					if ( plg.test( $el.val(), $el.attr('data-validate') ) ) {
+						plg.removeLabel( $el );
+					} else {
+						plg.addLabel( $el );
+						state.errors++;
+					}
+				},
+				submit: function (e) {
+					state.errors = 0;
+					$self.find('[data-validate]').each( function () {
+						plg.validate( $(this) );
+					} );
+					if (state.errors) {
+						e.preventDefault();
+					} else {
+						$self.trigger('submit');
+					}
+				}
+			};
+
+			plg.init();
+
+			return plg;
+		});
+	};
+
+})(jQuery);
